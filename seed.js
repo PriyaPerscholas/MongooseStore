3ad71c2625ba79586e12dea2e2ed20ff981e38eb
unknown <PriyaS@WI-CCP-ST-04.wilkescc.edu>
2022-03-18 10:29:48 -0400

    updated seed

--- seed.js
+++ seed.js
@@ -13,31 +13,31 @@ const productSeed =
                      qty: 5
               }, {
                      name: "Black Forest Cherry Cake",
-                     description: "Melts in your mouth. For Chocolate lovers",
+                     description: "Rich chocolate cake with cherry filling and iced in vanilla whipped topping. Garnished with marachino cherries and encased in shards of chocolate.",
                      img: "https://cutpcdnwimages.azureedge.net/images/products/90000/094894-600x600-A.jpg",
                      price: 35,
                      qty: 6
               }, {
                      name: "Vanilla Cream Cheese Cake",
-                     description: "Enjoy your cream cheese cake with floral design",
+                     description: "Six layers of vanilla cake filled and iced with alternating cream cheese and buttercream icing. Elegantly decorated with white chocolate bars, candies, and light pink drizzle",
                      img: "https://cutpcdnwimages.azureedge.net/images/products/95000/096723-600x600-A.jpg",
                      price: 45,
                      qty: 5,
               }, {
                      name: "Cookies and Cream Hero",
-                     description: "Three layers of chocolate and three layers of vanilla cake filled with...",
+                     description: "Three layers of chocolate and three layers of vanilla cake filled with cookies and cream mousse and iced with alternating stripes of mousse and buttercream. ",
                      img: "https://cutpcdnwimages.azureedge.net/images/products/95000/097253-600x600-A.jpg",
                      price: 50,
                      qty: 6,
               }, {
                      name: "Celestial Unicorn",
-                     description: "Six layers of vanilla or chocolate cake iced and filled with buttercre...",
+                     description: "Six layers of vanilla or chocolate cake iced and filled with buttercream icing. Covered with cosmic fusion sprinkles. Choose an option to fill the cake with sprinkles for extra magic. ",
                      img: "https://cutpcdnwimages.azureedge.net/images/products/95000/097251-600x600-A.jpg",
                      price: 35,
                      qty: 3
               }, {
                      name: "Rainbow Celebration",
-                     description: "Six layers of vanilla or chocolate cake filled with rainbow buttercrea...",
+                     description: "Six layers of vanilla or chocolate cake filled with rainbow buttercream colors. A deliciously fun cake with rainbow buttercream inside and out.",
                      img: "https://cutpcdnwimages.azureedge.net/images/products/90000/091297-600x600-A.jpg",
                      price: 55,
                      qty: 7
